009755c52544aa0034ef681e86ae205298712406
Riken Maharjan <rikenm@bu.edu>
2017-11-11 17:26:36 -0500

    databaseupdate2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'url'	: 'mongodb://localhost/gift_app'
+	//'url'	: 'mongodb://localhost/gift_app'
 	'url'	: 'mongodb://rikenm:kqQ-md8-qhG-tKS@ds257485.mlab.com:57485/faceappuserdata'
 	//mongodb://<dbuser>:<dbpassword>@ds257485.mlab.com:57485/faceappuserdata
 }
