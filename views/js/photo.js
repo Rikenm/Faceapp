5f1ceb12363145a627f8496a6ea5cac8e7bda181
Riken Maharjan <rikenm@bu.edu>
2017-11-11 23:27:33 -0500

    webcam correction7

--- views/js/photo.js
+++ views/js/photo.js
@@ -1,41 +1,54 @@
-(function(){
-  var video = document.getElementById('video'),
-    canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
-
-  vendorUrl = window.URL || window.webkitURL;
-  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia||
-                        navigator.mozGetUserMedia|| navigator.msGetUserMedia;
 
 
-
-  navigator.getMedia({
-    video:true,
-    audio: false
-  },function(stream){
-
-    video.src = vendorUrl.createObjectURL(stream);
-    video.play();
-  },function(error){
-
-  });
-
-  document.getElementById('capture').addEventListener('click',function(){
-         context.drawImage(video,0,0,400,300);
-         create_img();
-
-  });
-
-  function create_img(){
+window.onload = function() {
+    // all of your code goes in here
+    // it runs after the DOM is built
 
 
-    var x = document.createElement("IMG");
-    x.src = canvas.toDataURL("image/png"); // this will generate base64 data
-    document.getElementById("img_display").innerHTML = "<img src='"+x.src+"' width='400' height='300' class='img-responsive'>";
-    // document.body.appendChild(x);
-    //console.log(img.src);
-    }
+    (function(){
+      var video = document.getElementById('video'),
+        canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+
+      vendorUrl = window.URL || window.webkitURL;
+      navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia||
+                            navigator.mozGetUserMedia|| navigator.msGetUserMedia;
 
 
 
-})();
+      navigator.getMedia({
+        video:true,
+        audio: false
+      },function(stream){
+
+        video.src = vendorUrl.createObjectURL(stream);
+        video.play();
+      },function(error){
+
+      });
+
+      document.getElementById('capture').addEventListener('click',function(){
+             context.drawImage(video,0,0,400,300);
+             create_img();
+
+      });
+
+      function create_img(){
+
+
+        var x = document.createElement("IMG");
+        x.src = canvas.toDataURL("image/png"); // this will generate base64 data
+        document.getElementById("img_display").innerHTML = "<img src='"+x.src+"' width='400' height='300' class='img-responsive'>";
+        // document.body.appendChild(x);
+        //console.log(img.src);
+        }
+
+
+
+    })();
+
+
+
+
+
+}
