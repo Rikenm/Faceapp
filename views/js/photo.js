f78538bc1e90cd336fff183a49d9006f4fc357f4
Riken Maharjan <rikenm@bu.edu>
2017-11-12 07:27:28 -0500

    webcam correction24

--- views/js/photo.js
+++ views/js/photo.js
@@ -36,7 +36,7 @@ window.onload = function() {
 
         var x = document.createElement("IMG");
         x.src = canvas.toDataURL("image/png"); // this will generate base64 data
-        document.getElementById("img_dispplay").innerHTML = "<img src='"+x.src+"' width='380' height='300' class='img-responsive'>";
+        //document.getElementById("img_display").innerHTML = "<img src='"+x.src+"' width='380' height='300' class='img-responsive'>";
 
 
 
