1ac99edd2961e571490a2a30f241cb555a64c884
Riken Maharjan <rikenm@bu.edu>
2017-11-11 23:39:52 -0500

    webcam correction10

--- views/js/photo.js
+++ views/js/photo.js
@@ -1,7 +1,7 @@
 
 
 window.onload = function() {
-  
+
 
     (function(){
       var video = document.getElementById('video'),
@@ -26,7 +26,7 @@ window.onload = function() {
       });
 
       document.getElementById('capture').addEventListener('click',function(){
-             context.drawImage(video,0,0,400,300);
+             context.drawImage(video,0,0,300,300);
              create_img();
 
       });
@@ -36,7 +36,7 @@ window.onload = function() {
 
         var x = document.createElement("IMG");
         x.src = canvas.toDataURL("image/png"); // this will generate base64 data
-        document.getElementById("img_display").innerHTML = "<img src='"+x.src+"' width='400' height='300' class='img-responsive'>";
+        document.getElementById("img_display").innerHTML = "<img src='"+x.src+"' width='300' height='300' class='img-responsive'>";
         // document.body.appendChild(x);
         //console.log(img.src);
         }
